3a570081f4aea48fb0f58dbe168243ea12d38dd1
herrenchek <giselle.herr@icloud.com>
2019-08-25 19:33:41 -0400

    Updated

--- models/Article.js
+++ models/Article.js
@@ -18,7 +18,8 @@ var ArticleSchema = new Schema({
     required: true
   },
   summary: {
-    type: String
+    type: String,
+    required: true
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
